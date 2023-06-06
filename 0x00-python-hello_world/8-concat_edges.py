82a811994a14e62148feed3bf8ee1556d2ce7110
dhokabeatz <amglna2020@gmail.com>
2023-06-05 21:37:47 -0400

    Task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(str[39:66] + str[6] + str[107:111] + str[6]+ str[:6])
