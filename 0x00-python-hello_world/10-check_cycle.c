96bb3a1a3698343343079f82eb13f897b0ca0fa5
dhokabeatz <amglna2020@gmail.com>
2023-06-07 12:33:35 -0400

    Updated project 1 check cycle function for a more efficient code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,25 +2,24 @@
 #include "lists.h"
 
 /**
-* check_cycle - checks for a loop in a linked-list
-* @lists : listint_t pointer o the list
-* Return: 1 if there is a cycle or 0 if there is no cycle
-*/
-
-
+ * check_cycle - checks for a loop in a linked list
+ * @list: listint_t pointer to the list
+ *
+ * Return: 1 if there is a cycle, 0 if there is no cycle
+ */
 int check_cycle(listint_t *list)
 {
-    listint_t *s, *f;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    while (f != NULL && f.next != NULL)
-    {
-        s = s.next;
-        f = f.next.next;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (s == f)
-        {
-            return (1)
-        }
-    }
-    return (0);
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
 }
