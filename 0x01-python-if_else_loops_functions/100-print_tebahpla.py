5f09b91dda33a7c9fe1dd5eea4564b456e76bfc5
dhokabeatz <amglna2020@gmail.com>
2023-06-07 13:24:25 -0400

    Blank line removed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,4 +4,4 @@ output = ""
 for i in range(ord('z'), ord('a') - 1, -1):
     diff = 0 if i % 2 == 0 else 32
     output += '{}'.format(chr(i - diff))
-print('{}'.format(output))
+print('{}'.format(output),end=" ")
