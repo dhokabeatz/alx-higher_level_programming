e50d5c65a0d9686eded28af4b98db13aa9bbd8e7
dhokabeatz <amglna2020@gmail.com>
2023-06-07 06:23:16 -0400

    Replaced not with != and avoided storing characters

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-startPoint = 97
-endPoint = 123
-
-for letter in range(startPoint, endPoint):
-    if chr(letter) is not 'e' and chr(letter) is not 'q':
+for letter in range(97, 123):
+    if chr(letter) != 'e' and chr(letter) != 'q':
         print('{}'.format(chr(letter)), end='')
