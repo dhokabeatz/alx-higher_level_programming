568b1261b08391f5a3ab3e963360f68fea99fb48
dhokabeatz <amglna2020@gmail.com>
2023-06-11 16:22:07 -0400

    Added matrix printer file

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for col in row:
+                if col == row[-1]:
+                    print('{:d}'.format(col), end='')
+                else:
+                    print('{:d}'.format(col), end=' ')
+            print()
+    else:
+        print("Matrix doesn't exist")
