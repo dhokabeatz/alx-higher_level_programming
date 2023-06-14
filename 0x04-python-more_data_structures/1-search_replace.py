3318adb6f2a1c7462f4e9389552f66f5a872179f
dhokabeatz <amglna2020@gmail.com>
2023-06-14 19:21:10 -0400

    Updated 2 and 9

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """Searches for elements in the list and replaces them with the specified value."""
     def find_search(element):
         return element if element != search else replace
-    return [find_search(element) for element in my_list]
+    return list(map(find_search, my_list))
