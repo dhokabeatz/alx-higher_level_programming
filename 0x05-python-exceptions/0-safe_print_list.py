f45cc282987b07b1b79914b2cf3ab8182a7b51bc
dhokabeatz <amglna2020@gmail.com>
2023-06-26 18:01:41 -0400

    Made try/except interior

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
     elementCount = 0
-
-    try:
-        for number in range(x):
-            print("{}".format(my_list[number]), end="")
+    for number in range(x):
+        try:
+            print(f"{my_list[number]}", end="")
             elementCount += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-        return elementCount
+        except IndexError:
+            break
+    print()
+    return (elementCount)
